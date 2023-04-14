3e1127b23104236886f285686c0598892247b94d
TheGenio <s202164170@kfupm.edu.sa>
2023-04-14 07:13:53 +0300

    4th commit
    Big changes:
    Added a new class, banker
    Added a new log in system. All new Clients will be saved into a txt file. So, when the code runs it will remember who signed up and when they try to log in it will accept them
    
    Small changes:
    Added and changed some minor bits

---
+++ src/banker.java
@@ -0,0 +1,21 @@
+import java.io.Serializable;
+import java.util.List;
+
+public class banker extends client implements Serializable {
+
+    private List<client> clients;
+    banker(List<client> clients, String name, String password) {
+        super(0, name, password);
+        this.clients = clients;
+    }
+
+
+    public void GetAllUsers(){
+        for(client client : clients){
+            System.out.println("Clients are: " + client);
+        }
+
+    }
+
+
+}
